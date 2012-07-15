efac11ff6e8e321ec4948060c0ef2e4890e0a2ab
Pranav Ravichandran <prp.1111@gmail.com>
2012-07-15 11:23:12 +0530

    Added functionality for previewing changes.

--- pub.py
+++ pub.py
@@ -1,17 +1,23 @@
 import fileinput
 import datetime
+from flask import *
 
-def contentDump(content):
+def contentDump(content, outfile):
 	insertContent = False
-	for line in fileinput.input('content/index.html', inplace=1):
+	for line in fileinput.input(outfile, inplace=1):
 	  if line.startswith('<div class="content">'):
 	    insertContent = True
 	  else:
 	    if insertContent:
-	      print '<hr /><p>' + content + '<br />' + str(datetime.datetime.now()) + '</p>'
+	      print '<hr /><p>' + content + '<br /><div class="date">' + str(datetime.datetime.now()) + '</div></p>'
 	    insertContent = False
 	  print line,
 
-def publish(content):
-	contentDump(content)
-	return 'Published'
+def publish(content, preview):
+	if preview:
+		contentDump(content, 'templates/preview.html')
+		return '<a href="%s" target="_blank">Preview</a>' % url_for('show_preview')
+	else:
+		contentDump(content, 'templates/index.html')
+		contentDump(content, 'content/index.html')
+		return '<a href="%s" target="_blank">Published</a>' % url_for('show_pubbed')
--- server.py
+++ server.py
@@ -3,16 +3,29 @@
 from flask import *
 from pub import *
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route("/", methods = ['GET','POST'])
 
 def index():
 	if request.method == 'GET':
-		return render_template('index.html')
+		return render_template('layout.html')
 	if request.method == 'POST':
 		content = request.form['content']
-		return publish(content)
+		preview = request.form['preview']
+		if preview == 'True':
+			return publish(content, True)
+		return publish(content, False)
+
+@app.route("/preview")
+
+def show_preview():
+	return render_template('preview.html')
+
+@app.route("/pubbed")
+
+def show_pubbed():
+	return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run(debug = True)
