692cd2b8719c03f9915e4cd7fea44776ed55b81a
Pranav Ravichandran <prp.1111@gmail.com>
2012-09-29 23:02:53 +0530

    Initial database populating setup.

---
+++ setup.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from models import *
+import os
+
+class Data:
+	def __init__(self):
+		self.username = str(raw_input("Enter your username: "))
+		self.password = str(raw_input("Enter your password: "))
+
+		while True:
+			self.confPass = str(raw_input("Confirm your password: "))
+			if self.confPass == self.password:
+				break
+	
+		self.title = str(raw_input("Blog title? "))
+		self.desc = str(raw_input("Blog Description? "))
+
+		while True:
+			self.layout = str(raw_input("Bottom-to-Top (press y) or Top-to-Bottom (press n)? (Default: Bottom to Top) "))
+
+			if self.layout == 'y' or self.layout == '':
+				self.layout = 1
+			elif self.layout == 'n':
+				self.layout = 0
+			else:
+				continue
+
+			break
+
+if __name__ == "__main__":
+	if os.path.exists(DB_URI):
+		confirm = str(raw_input("User Database exists already. Do you want to re-configure? This will erase your current database configuration. Confirm (y/n) "))
+		while True:
+			if confirm == 'y' or confirm == 'Y':
+				os.remove(DB_URI)
+				break
+			elif confirm == 'n' or confirm == 'N':
+				sys.exit(1)
+			else:
+				confirm = str(raw_input('Press y or n. '))
+				continue
+
+	a = Data()
+	db.create_all()
+	user = User(a.username, a.password)
+	db.session.add(user)
+	db.session.commit()
+	print User.query.all()
