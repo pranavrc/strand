1f8296e49b63546166426161635973cd6b9f94b0
Pranav Ravichandran <prp.1111@gmail.com>
2012-09-30 08:39:20 +0530

    Form validation and redirecting back to login page from content publishing tool if session cookie's absent.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import *
 from pub import *
-from setup import app
+from setup import *
 
 @app.route("/login", methods = ['GET','POST'])
 
@@ -10,13 +10,29 @@ def login():
 	if request.method == 'GET':
 		return render_template('login.html')
 	if request.method == 'POST':
-		return 'foo'
+		usern = request.form['username']
+		passw = request.form['password']
+		
+		if User.query.filter_by(username = usern).first():
+			if User.query.filter_by(password = passw).first():
+				flash('Successful login.')
+				session['username'] = usern
+				return redirect(url_for('index'))
+			else:
+				error = 'Invalid Password.'
+		else:
+			error = 'Invalid Username.'
+
+		return render_template('login.html', error = error)
 
 @app.route("/", methods = ['GET','POST'])
 
 def index():
 	if request.method == 'GET':
-		return render_template('layout.html')
+		if 'username' in session:
+			return render_template('layout.html')
+		else:
+			return redirect(url_for('login'))
 	if request.method == 'POST':
 		content = request.form['content']
 		preview = request.form['preview']
